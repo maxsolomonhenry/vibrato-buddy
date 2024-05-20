81b9443e7b1b6dbf11d3e03c8c52acaa1c6dd3d2
Max Henry <mhenry@eers.ca>
2024-05-20 15:16:00 -0400

    pitch tracks at much lower sample rate

--- src/data_util.py
+++ src/data_util.py
@@ -1,15 +1,32 @@
 import numpy as np
 import librosa
 
-def hz_to_audio(hz,sr=44100):
+
+def hz_to_audio(hz,sr=44100, hop_size=None):
+
+    if hop_size:
+        hz = np.repeat(hz, hop_size)
+
     phase = np.cumsum(2 * np.pi * hz / sr)
     return np.cos(phase)
 
-def df_to_hz(df):
+def df_to_hz(df, new_sr=None):
+
+    NATIVE_SR = 44100
+
+    if new_sr:
+        df['start_time'] = np.round(df['start_time'] / NATIVE_SR * new_sr).astype(int)
+        df['end_time'] = np.round(df['end_time'] / NATIVE_SR * new_sr).astype(int)
+
     max_time = df['end_time'].max()
+
+
     f0_x = np.full(max_time + 1, np.nan)
 
-    for _, row in df.iterrows():    
+    for _, row in df.iterrows():
         f0_x[row['start_time']:row['end_time']] = row['note']
 
     return librosa.midi_to_hz(f0_x)
+
+def pow_to_db(x):
+    return 10.0 * np.log10(x)
